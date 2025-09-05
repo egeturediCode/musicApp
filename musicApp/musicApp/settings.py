11e74613bebc8bc786e400b57c4b52968481fddb
ege türedi <egemen.turedi81@gmail.com>
2025-09-06 00:36:01 +0300

    update

--- musicApp/musicApp/settings.py
+++ musicApp/musicApp/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fhiw4o3kjeiz0q4zfljj6o%wips2&44(!_r-+czw0s56sq^e*+'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
